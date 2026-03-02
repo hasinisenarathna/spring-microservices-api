7adc12a170a1d4f04973f66b4c7a85e3263adc94
Hasini Senarathna <hasini8888z@gmail.com>
2026-03-02 23:18:05 +0530

    first update

---
+++ order-service/src/test/java/com/hasini/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasini.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
